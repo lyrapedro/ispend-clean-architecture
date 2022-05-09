cefb5b428a36e9fc207e93f41b50d752e1e1d087
Pedro Lyra <pedrolyradev@outlook.com>
2022-05-09 02:59:06 -0300

    react front-end app

--- iSpend.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace iSpend.WebUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
