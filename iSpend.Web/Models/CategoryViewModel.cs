30106e575115265dfe559c3f1f4de67826a21e02
lyrapedro <pedrolyradev@outlook.com>
2022-06-18 20:21:33 -0300

    configuration

---
+++ iSpend.Web/Models/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace iSpend.Web.Models
+{
+    public class CategoryViewModel: Entity
+    {
+        public string? UserId { get; private set; }
+        public string Name { get; private set; }
+        public string Color { get; private set; }
+    }
+}
---
+++ iSpend.Web/Models/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace iSpend.Web.Models
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
