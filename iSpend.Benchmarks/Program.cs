02617cabad8888bd40ac8adb278b1bfa202a417f
lyrapedro <pedrolyradev@outlook.com>
2023-01-04 01:07:16 -0300

    benchmark project to test performance

---
+++ iSpend.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace iSpend.Benchmarks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        new BenchmarkSwitcher(new [] { typeof(Benchmark) }).Run(args);
+    }
+}
